2146866ecb972ebc47e2d37a895fae51d5555633
jogliast <jogliast@cuc.edu.co>
2024-09-29 23:34:10 -0500

    primera version

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import miImagen from "./assets/img/Imagen-de-WhatsApp-2024-09-29-a-las-17.17.02_50602a8b.ico"
+
+function App() {
+  return (
+    <>
+     <div>
+      <img src={miImagen} alt='logo viejo cuc' />
+      <h1>!!Bienvenidoa a mi proyecto¡¡</h1>
+      <p>Espero les guste, con fotos de mi perrito</p>
+     </div>
+    </>
+  );
+}
+
+export default App;
